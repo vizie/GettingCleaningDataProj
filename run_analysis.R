b9bc7271dd839afbb9c29dc5899aa33522d08264
vizie <ecvolz.app@gmail.com>
2015-10-25 17:57:35 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -13,34 +13,33 @@ subTrain<-fread("UCI HAR Dataset/train/subject_train.txt")
 subTest<-fread("UCI HAR Dataset/test/subject_test.txt")
 
 features<-fread("UCI HAR Dataset/features.txt")
-setkey(features, "V1")
+#setkey(features, "V1")
 
 activityLabels<-fread("UCI HAR Dataset/activity_labels.txt")
-setkey(activityLabels, "V1")
-setnames(activityLabels, 2, "activityLabel") #Rename the new loaded column
+names(activityLabels)<-c("actID", "activityLabel")
+setkey(activityLabels, "actID")
+
 
 yTrain<-fread("UCI HAR Dataset/train/y_train.txt")
-setkey(yTrain, "V1")
-yTrain<-yTrain[activityLabels]
+
 
 yTest<-fread("UCI HAR Dataset/test/y_test.txt")
-setkey(yTest, "V1")
-yTest<-yTest[activityLabels]
+
 
 #Create the Key Columns
-keycols<-c("subject", "activityLabel")
+keycols<-c("subject", "actID")
 #Get test data
 xTest<-fread("UCI HAR Dataset/test/x_test.txt")
 #Add activity and subject columns
 xTest[,subject:=subTest[,V1]]
-xTest[,activityLabel:=yTest[,activityLabel]]
+xTest[,actID:=yTest[,V1]]
 setkeyv(xTest, keycols)
 
 #Get training data
 xTrain<-fread("UCI HAR Dataset/train/x_train.txt")
 #Add activity and subject columns
 xTrain[,subject:=subTrain[,V1]]
-xTrain[,activityLabel:=yTrain[,activityLabel]]
+xTrain[,actID:=yTrain[,V1]]
 setkeyv(xTrain, keycols)
 
 #Append the data tables
@@ -50,8 +49,10 @@ names(xFull)<-c(features$V2,keycols)
 setkeyv(xFull, keycols)
 
 #Subset the data to on include columns with Mean, Std, or the Keys in their title 
-meandt<-xFull[, c(grep("mean",names(xFull)),grep("std",names(xFull)),grep("subject",names(xFull)),grep("activityLabel",names(xFull))),with=FALSE]
+meandt<-xFull[, c(grep("mean",names(xFull)),grep("std",names(xFull)),grep("subject",names(xFull)),grep("actID",names(xFull))),with=FALSE]
+#meandt <- subset(reduced_data, select = -grep("meanFreq",names(reduced_data)))
 #Summarize the data by subject and activity
 output<-meandt[,lapply(.SD,mean),by=keycols]
+output<-activityLabels[output]
 #Report on Means and std
 write.table(output, "mean.txt", row.name=FALSE)
