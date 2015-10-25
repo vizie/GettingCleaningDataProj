e0704e8b01c628b130627e866742b88f888094bf
vizie <ecvolz.app@gmail.com>
2015-10-25 18:27:16 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -13,11 +13,13 @@ subTrain<-fread("UCI HAR Dataset/train/subject_train.txt")
 subTest<-fread("UCI HAR Dataset/test/subject_test.txt")
 
 features<-fread("UCI HAR Dataset/features.txt")
-#setkey(features, "V1")
+
 
 activityLabels<-fread("UCI HAR Dataset/activity_labels.txt")
 names(activityLabels)<-c("actID", "activityLabel")
 setkey(activityLabels, "actID")
+
+#setnames(activityLabels, 2, "activityLabel") #Rename the new loaded column
 
 
 yTrain<-fread("UCI HAR Dataset/train/y_train.txt")
@@ -34,25 +36,26 @@ xTest<-fread("UCI HAR Dataset/test/x_test.txt")
 xTest[,subject:=subTest[,V1]]
 xTest[,actID:=yTest[,V1]]
 setkeyv(xTest, keycols)
-
+xTest<-merge(xTest, activityLabels, by="actID", all=TRUE)
 #Get training data
 xTrain<-fread("UCI HAR Dataset/train/x_train.txt")
 #Add activity and subject columns
 xTrain[,subject:=subTrain[,V1]]
 xTrain[,actID:=yTrain[,V1]]
 setkeyv(xTrain, keycols)
+xTrain<-merge(xTrain, activityLabels, by="actID", all=TRUE)
 
 #Append the data tables
 xFull<-rbindlist(list(xTest, xTrain))
 #Set column names and keys for the aggregate file
-names(xFull)<-c(features$V2,keycols)
+names(xFull)<-c("actID",features$V2,"subject","activityLabel")
 setkeyv(xFull, keycols)
 
 #Subset the data to on include columns with Mean, Std, or the Keys in their title 
-meandt<-xFull[, c(grep("mean",names(xFull)),grep("std",names(xFull)),grep("subject",names(xFull)),grep("actID",names(xFull))),with=FALSE]
-#meandt <- subset(reduced_data, select = -grep("meanFreq",names(reduced_data)))
+meandt<-xFull[, c(grep("mean",names(xFull)),grep("std",names(xFull)),grep("subject",names(xFull)),grep("activityLabel",names(xFull)),grep("subject",names(xFull))),with=FALSE]
+meandt<-meandt[, c(-grep("meanF",names(meandt))),with=FALSE]
 #Summarize the data by subject and activity
-output<-meandt[,lapply(.SD,mean),by=keycols]
-output<-activityLabels[output]
+output<-meandt[,lapply(.SD,mean),by=c("subject","activityLabel")]
+
 #Report on Means and std
 write.table(output, "mean.txt", row.name=FALSE)
